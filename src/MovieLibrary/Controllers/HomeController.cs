2306401f7f3e8e9fc15d13598ffe6e065a7bd869
CodeFuller <CodeFuller@gmail.com>
2020-04-21 08:19:12 +0300

    Integrate code analyzers

--- src/MovieLibrary/Controllers/HomeController.cs
+++ src/MovieLibrary/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MovieLibrary.Models;
@@ -11,11 +8,11 @@ namespace MovieLibrary.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		private readonly ILogger<HomeController> logger;
 
 		public HomeController(ILogger<HomeController> logger)
 		{
-			_logger = logger;
+			this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		public IActionResult Index()
--- src/MovieLibrary/Models/ErrorViewModel.cs
+++ src/MovieLibrary/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace MovieLibrary.Models
 {
 	public class ErrorViewModel
