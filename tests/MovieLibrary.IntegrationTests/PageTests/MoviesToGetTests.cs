86661fc2dac64edd0d9a9d77238fde76fc127c56
CodeFuller <CodeFuller@gmail.com>
2020-05-09 17:59:21 +0300

    Add snapshot tests for movie changing state confirmation pages

--- tests/MovieLibrary.IntegrationTests/PageTests/MoviesToGetTests.cs
+++ tests/MovieLibrary.IntegrationTests/PageTests/MoviesToGetTests.cs
@@ -43,6 +43,38 @@ namespace MovieLibrary.IntegrationTests.PageTests
 		}
 
 		[TestMethod]
+		public async Task ConfirmMovingToSeePage_ForAdministratorAccount_IsLoadedCorrectly()
+		{
+			// Arrange
+
+			using var client = CreateHttpClient(UserRoles.AdministratorRoles);
+
+			// Act
+
+			using var response = await client.GetAsync(new Uri("https://localhost:5001/MoviesToGet/ConfirmMovingToSee/5eac4f407a15596e90c09d7b"), CancellationToken.None);
+
+			// Assert
+
+			await ResponseAssert.VerifyPageLoaded(response);
+		}
+
+		[TestMethod]
+		public async Task ConfirmMovingToSeePage_ForUserAccount_RedirectsToAccessDeniedPage()
+		{
+			// Arrange
+
+			using var client = CreateHttpClient(UserRoles.LimitedUserRoles);
+
+			// Act
+
+			using var response = await client.GetAsync(new Uri("https://localhost:5001/MoviesToGet/ConfirmMovingToSee/5eac4f407a15596e90c09d7b"), CancellationToken.None);
+
+			// Assert
+
+			ResponseAssert.VerifyRedirect(response, new Uri("https://localhost:5001/Identity/Account/AccessDenied?ReturnUrl=%2FMoviesToGet%2FConfirmMovingToSee%2F5eac4f407a15596e90c09d7b"));
+		}
+
+		[TestMethod]
 		public async Task ConfirmMovieDeletionPage_ForAdministratorAccount_IsLoadedCorrectly()
 		{
 			// Arrange
--- tests/MovieLibrary.IntegrationTests/PageTests/MoviesToSeeTests.cs
+++ tests/MovieLibrary.IntegrationTests/PageTests/MoviesToSeeTests.cs
@@ -43,6 +43,38 @@ namespace MovieLibrary.IntegrationTests.PageTests
 		}
 
 		[TestMethod]
+		public async Task ConfirmMarkingAsSeenPage_ForAdministratorAccount_IsLoadedCorrectly()
+		{
+			// Arrange
+
+			using var client = CreateHttpClient(UserRoles.AdministratorRoles);
+
+			// Act
+
+			using var response = await client.GetAsync(new Uri("https://localhost:5001/MoviesToSee/ConfirmMarkingAsSeen/5ead62d14be68246b45bba82"), CancellationToken.None);
+
+			// Assert
+
+			await ResponseAssert.VerifyPageLoaded(response);
+		}
+
+		[TestMethod]
+		public async Task ConfirmMarkingAsSeenPage_ForUserAccount_RedirectsToAccessDeniedPage()
+		{
+			// Arrange
+
+			using var client = CreateHttpClient(UserRoles.LimitedUserRoles);
+
+			// Act
+
+			using var response = await client.GetAsync(new Uri("https://localhost:5001/MoviesToSee/ConfirmMarkingAsSeen/5ead62d14be68246b45bba82"), CancellationToken.None);
+
+			// Assert
+
+			ResponseAssert.VerifyRedirect(response, new Uri("https://localhost:5001/Identity/Account/AccessDenied?ReturnUrl=%2FMoviesToSee%2FConfirmMarkingAsSeen%2F5ead62d14be68246b45bba82"));
+		}
+
+		[TestMethod]
 		public async Task ConfirmMovieDeletionPage_ForAdministratorAccount_IsLoadedCorrectly()
 		{
 			// Arrange
