1ffb17ad6610c60338ee83deb820f339c06b77c5
CodeFuller <CodeFuller@gmail.com>
2021-03-29 08:55:09 +0300

    Add IT for MovieUniquenessChecker

---
+++ tests/MovieLibrary.Logic.IntegrationTests/Services/MovieUniquenessCheckerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieLibrary.Logic.Interfaces;
+using MovieLibrary.Logic.Models;
+using static MovieLibrary.Logic.IntegrationTests.TestsBootstrapper;
+
+namespace MovieLibrary.Logic.IntegrationTests.Services
+{
+	[TestClass]
+	public class MovieUniquenessCheckerTests
+	{
+		[TestMethod]
+		public async Task CheckMovie_MovieIsUnique_ReturnsMovieIsUnique()
+		{
+			// Arrange
+
+			var serviceProvider = await BootstrapTests(seedData: true);
+			var target = serviceProvider.GetRequiredService<IMovieUniquenessChecker>();
+
+			// Act
+
+			var result = await target.CheckMovie(new Uri("https://www.kinopoisk.ru/film/12345/"), CancellationToken.None);
+
+			// Assert
+
+			Assert.AreEqual(MovieUniquenessCheckResult.MovieIsUnique, result);
+		}
+
+		[TestMethod]
+		public async Task CheckMovie_MovieExistsInMoviesToGet_ReturnsExistsInMoviesToGet()
+		{
+			// Arrange
+
+			var serviceProvider = await BootstrapTests(seedData: true);
+			var target = serviceProvider.GetRequiredService<IMovieUniquenessChecker>();
+
+			// Act
+
+			var result = await target.CheckMovie(new Uri("https://www.kinopoisk.ru/film/777/"), CancellationToken.None);
+
+			// Assert
+
+			Assert.AreEqual(MovieUniquenessCheckResult.ExistsInMoviesToGet, result);
+		}
+
+		[TestMethod]
+		public async Task CheckMovie_MovieExistsInMoviesToSee_ReturnsExistsInMoviesToSee()
+		{
+			// Arrange
+
+			var serviceProvider = await BootstrapTests(seedData: true);
+			var target = serviceProvider.GetRequiredService<IMovieUniquenessChecker>();
+
+			// Act
+
+			var result = await target.CheckMovie(new Uri("https://www.kinopoisk.ru/film/888/"), CancellationToken.None);
+
+			// Assert
+
+			Assert.AreEqual(MovieUniquenessCheckResult.ExistsInMoviesToSee, result);
+		}
+	}
+}
